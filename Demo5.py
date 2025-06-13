852adc3603d3419703c56167928fc24316411b91
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-13 13:12:32 +0600

    Add files via upload

---
+++ Demo5.py
@@ -0,0 +1,23 @@
+# Iterate a Pandas DataFrame to display the column names
+# Code by StudyOpedia
+
+import pandas as pd
+
+#DataSet
+
+data = {
+    'Student': ["Israt", "Asma", "Nitu", "Sathi", "Riya"],
+    'Rank': [1, 4, 3, 5, 2],
+    'Marks': [95, 70, 80, 60, 90]
+}
+
+# Use the index argument to set your indexes
+
+df = pd.DataFrame(data,index = ['Student1','Student2','Student3','Student4','Student5'])
+
+print("Student Records\n\n", df)
+
+#Iterating
+print("\nDisplaying the columns:")
+for col in df:
+    print(col)
