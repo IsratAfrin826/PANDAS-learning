108924207bf3a66f52f234d19b1b2c863f284079
Mosammat Israt Arefin Mazumder <nisratafrin826@gmail.com>
2025-06-24 11:34:12 +0600

    Add files via upload

---
+++ Demo11(T).py
@@ -0,0 +1,19 @@
+# Pandas DataFrame T attribute
+# Code by Studyopedia
+
+import pandas as pd
+
+#Dataset
+
+data = {
+    'student' : ["Israt","Janie","Anika","Shomi","Rimi"],
+    'Rank' : [1,4,3,5,2],
+    'Marks' : [95,70,80,60,90]
+}
+
+# Create a DataFrame using the DataFrame() method with index
+df = pd.DataFrame(data, index=['RowA','RowB','RowC','RowD','RowE'],)
+
+print("Student Records\n\n", df)
+
+print("\nTranspose:\n",df.T)
